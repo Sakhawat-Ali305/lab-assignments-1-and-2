dacd38d468e67f3e4e61068d81de01333fc6278c
Sakhawat-Ali305 <147754258+Sakhawat-Ali305@users.noreply.github.com>
2023-10-12 11:51:10 -0700

    Add files via upload

---
+++ SAKHAWAT ALI ( 470935) SEC-C.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//                                   TASK 01
+/*int x2,x1,y2,y1,d1,d2,d;
+cout<<" Enter the value of x1 ";
+cin>>x1;
+cout<<" Enter the value of x2 ";
+cin>>x2;
+cout<<" Enter the value of y1 ";
+cin>>y1;
+cout<<" Enter the value of y2 ";
+cin>>y2;
+d1=(x2-x1);
+d2=(y2-y1);
+d=d1*d1+d2*d1;
+cout<<" The distance between your points is: "<<d;
+
+return 0;*/
+
+//                                TASK 3
+/*int a ,b ,p ;
+cout<<" inter the value of a ";
+cin>>a;
+cout<<" inter the value of b ";
+cin>>b;
+p=a*a+2*a*b+b*b;
+cout<<" the value of p is: "<<p;*/
+
+//                                                 task   4
+
+/*float f ,c ;
+cout<<"your value of f ";
+cin>>f ;
+c=(f-32)*5/9;
+cout<<"your value of c :" <<c;*/
+
+//                                             LAB MANUAL 2
+//                                             LAB TASK 1
+/*int x; 
+cout<<"inter age of person"; 
+cin>>x;
+if (x>=18){
+	cout<<"eligible for casting vote";
+}
+ 
+else if (x<18){
+	cout<<"not eligible for casting vote  ";
+}*/
+
+ //                                                       LAB TASK 2
+ 
+/* int x;
+ cout <<"your value of x";
+ cin>>x;
+ if (10<=x&&x<=50){
+ cout<<"fall within 10 to 50";
+}
+ else {
+ 	cout<<"doesnot fall within the range";
+ }*/
+ 
+ //                                                   HOME TASK 1
+ 
+/* int x , a, b, c, d, f ;
+ cout<<"enter your value of x";
+ cin>>x;
+if (90<=x&&x<=100){
+	cout<<"A grade"; 
+}
+else if (75<=x&&x<90) {
+	cout<<"B grade";
+}
+else if (60<=x&&x<75){
+	cout<<"C grade";
+}
+else if (45<=x&&x<60){
+	cout<<" D grade ";
+}
+else if (0<=x&&x<45){
+	cout<<" F grade";
+}*/
+
+//                                                  HOME TASK 2
+
+/*int x;
+cout<<" Enter your value of x";
+cin>>x;
+if (x%2==0&&x%5==0 )
+{
+  cout<<"number is even and divisible by 5";
+}
+ else{
+ 	cout<<"mentioned number dosenot fall in required category";
+ }*/
+ //                                          home task 3
+ 
+ /*int x ;
+ cout<<"inter your value of x";
+ cin>>x;
+ if (x%4==0){
+ 	cout<<" leap year ";
+ }
+ else {
+ 	cout<<" not leap year ";
+ }*/
+ //                                         HOME TASK 04
+ 
+    /*double gpa;
+    double attendance;
+
+    cout << "Enter GPA: ";
+    cin >> gpa;
+
+    cout << "Enter attendance percentage: ";
+    cin >> attendance;
+
+    if (gpa >= 3.5 && attendance >= 80) {
+        std::cout << "Congratulations! You are eligible for a scholarship." << std::endl;
+    } else {
+        std::cout << "Sorry, you are not eligible for a scholarship." << std::endl;
+    }
+
+    return 0;*/
+ 
+ //                             HOME TASK 05
+ /*char character;
+
+    cout << "Enter a character: ";
+    cin >> character;
+
+    character = tolower(character);
+
+    if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u'){
+	
+        cout << character << " is a vowel." << endl;
+    } 
+    else {
+        cout << "alphabet is consonant" << endl;
+    }
+
+    return 0;*/
+ 
+ //                             LAB TASK 03
+ 
+ /*   int num1, num2;
+
+    cout << "Enter the first integer: ";
+    cin >> num1;
+
+    cout << "Enter the second integer: ";
+    cin >> num2;
+
+    if (num1 > num2) {
+        cout << "The maximum value is: " << num1 << endl;
+    } else if (num2 > num1) {
+        cout << "The maximum value is: " << num2 << endl;
+    } else {
+        cout << "Both integers are equal." << endl;
+    }
+
+    return 0;*/
+    //                                   LAB TASK 05
+	
+    /*double score1, score2, score3;
+    double average;
+
+    cout << "Enter the score for the first exam: ";
+    cin >> score1;
+
+    cout << "Enter the score for the second exam: ";
+    cin >> score2;
+
+    cout << "Enter the score for the third exam: ";
+    cin >> score3;
+
+    average = (score1 + score2 + score3) / 3.0;
+
+    cout << "The average score is: " << average << endl;
+
+    if (average >= 60) {
+        cout << "Congratulations! You have passed." << endl;
+    } else {
+        cout << "Sorry, you have not passed." << endl;
+    }
+
+    return 0;*/
+
+}
